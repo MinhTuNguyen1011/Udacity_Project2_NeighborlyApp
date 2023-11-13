5281fcf93e81c8404cb8cc973b93e8cefe56cc74
MinhTuNguyen1011 <41544897+MinhTuNguyen1011@users.noreply.github.com>
2023-11-13 16:45:22 +0700

    Update __init__.py

--- NeighborlyAPI/getPost/__init__.py
+++ NeighborlyAPI/getPost/__init__.py
@@ -10,9 +10,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://azurecosmosdbneighborly:SDnHk2IYrtaCuYzcp75TIZYqbqvu6C4tVPVRMWNXWLmtMyJHTXsnpLUaOJHFgDIQTl1rQ4LTId7lACDbowSl7w==@azurecosmosdbneighborly.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@azurecosmosdbneighborly@"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb+srv://trinhcao2516:HXTjpuDO70W6TQ3E@cluster0.yg5bjc4.mongodb.net/?retryWrites=true&w=majority"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
-            database = client['neighborly']
+            database = client['udacity']
             collection = database['posts']
 
             query = {'_id': str(id)}
